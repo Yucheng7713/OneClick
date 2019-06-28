f9c349ca6d187852b35dcba7e841c7e01d0ddaca
Yucheng7713 <yuchengh@usc.edu>
2019-06-28 14:35:36 -0700

    Edited work dirt in sh_scripts

--- sh_scripts/down.sh
+++ sh_scripts/down.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker exec `docker ps -q -l` /bin/bash -c \
-"cd terraform; terraform destroy --auto-approve"
+"cd /;cd terraform; terraform destroy --auto-approve"
--- sh_scripts/local_deploy.sh
+++ sh_scripts/local_deploy.sh
@@ -3,20 +3,15 @@
 # Set build image name/tag
 FLASK_IMAGE_TAG="$DOCKER_USER_NAME/oneclick-flaskapp"
 
-LOCAL_PATH=$1
+# Clone the deploying project to app then build and push image to Docker Hub
+rm -rf app
+cp -R $1 ./app
+cp ./app_build_template/Dockerfile ./app/Dockerfile \
+&& docker build -t $FLASK_IMAGE_TAG ./app/. \
+&& docker push $FLASK_IMAGE_TAG
+# Deploy Flask app image upon the Kubernetes cluster
+docker exec `docker ps -q -l` /bin/bash -c \
+"cat \"./k8s/flask-deploy-template.yaml\" | sed \"s/{{NAME_OF_IMAGE}}/$(echo $FLASK_IMAGE_TAG | perl -pe 's/\//\\\//g')/g\" > ./k8s/flask-app-deployment.yaml"
 
-if [[ -n "$LOCAL_PATH" ]]; then
-	# Clone the deploying project to app then build and push image to Docker Hub
-	cp -R $LOCAL_PATH ./app
-	cp ./app_build_template/Dockerfile ./app/Dockerfile \
-	&& docker build -t $FLASK_IMAGE_TAG ./app/. \
-	&& docker push $FLASK_IMAGE_TAG
-	# Deploy Flask app image upon the Kubernetes cluster
-	docker exec `docker ps -q -l` /bin/bash -c \
-	"cat \"./k8s/flask-deploy-template.yaml\" | sed \"s/{{NAME_OF_IMAGE}}/$(echo $FLASK_IMAGE_TAG | perl -pe 's/\//\\\//g')/g\" > ./k8s/flask-app-deployment.yaml"
-
-	docker exec `docker ps -q -l` /bin/bash -c \
-	"cd terraform; terraform apply -target=null_resource.app_deploy --auto-approve"
-else
-	echo "Please specify directory path."
-fi
+docker exec `docker ps -q -l` /bin/bash -c \
+"cd /;cd terraform; terraform apply -target=null_resource.app_deploy --auto-approve"
